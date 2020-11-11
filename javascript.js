56064fdacfaac1c4558a0ca159489f29b97d73fe
Sergey Semushin <s.semushin@innopolis.university>
2020-11-11 20:38:06 +0300

    Refactoring

--- javascript.js
+++ javascript.js
@@ -1,9 +1,12 @@
 // constants
 
 var wind = -1, snowhillHeight = 3, fallingRate = 1/30, meltingRate = 15/1000, speed = 6;
+
 var ballSymbols = ['/', '\\', '_', '"', '<', '>', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var ballColors = ['purple', 'brown', 'orange', 'tomato', 'orchid', 'navy', 'fuchsia'];
+textColors = Array.from(ballColors);
 var treeSymbolsColors = {'M': 'green', '|': 'saddlebrown'};
+starColor = 'red';
 
 
 
@@ -14,8 +17,6 @@ function randomElement(arr) {
 }
 
 var moveSnowInterval = null;
-var textColors = [];
-Object.assign(textColors, ballColors);
 var treeSymbols = Object.getOwnPropertyNames(treeSymbolsColors);
 
 
@@ -35,14 +36,16 @@ window.onload = function() {
 		if (ballSymbols.indexOf(picture[i]) >= 0) {
 			symbol = picture[i];
 			
-			if (i >= picture.indexOf(treeSymbols[0])) {
-				if (picture[i-1] == treeSymbols[0] && picture[i] == ballSymbols[0]) {
+			if (i < picture.indexOf(treeSymbols[0])) {
+				color = starColor;
+			}
+			else {
+				if (picture[i-1] == treeSymbols[0] && picture[i] == ballSymbols[0]) { // if new ball
 					previousColor = randomElement(ballColors);
 					ballColors.splice(ballColors.indexOf(previousColor), 1);
 				}
 				color = previousColor;
-			}
-			else color = 'red';
+			};
 		}
 		else if (treeSymbols.indexOf(picture[i]) >= 0) {
 			symbol = picture[i];
@@ -61,9 +64,6 @@ window.onload = function() {
 		}
 	}
 	document.getElementById('picture').hidden = true;
-
-	// see below
-	window.onresize();
 	
 	// blinking h1 text
 	var H = document.getElementById('H');
@@ -74,60 +74,60 @@ window.onload = function() {
 	}
 	blinkText(0);
 
-}
+	// window size dependent code
+	window.onresize = function() {
 
+		// calculate size
+		width = Math.floor(window.innerWidth/8.5);
+		height = Math.floor(window.innerHeight/18);
+		
+		// init empty space for snow
+		var snowBlock = document.getElementById('snow');
+		snowBlock.innerHTML = '';
+		var snow = Array(height).fill(null);
+		snow = snow.map(function(){return Array(width).fill(' ');});
 
-
-window.onresize = function() {
-
-	// calculate size
-	width = Math.floor(window.innerWidth/8.5);
-	height = Math.floor(window.innerHeight/18);
-	
-	// init empty space for snow
-	var snowBlock = document.getElementById('snow');
-	snowBlock.innerHTML = '';
-	var snow = Array(height).fill(null);
-	snow = snow.map(function(){return Array(width).fill(' ');});
-
-	// randomize initial snow
-	for (var i = 0; i <= 2*height; i++) {
-		moveSnow();
-	}
-	
-	// start animation
-	function moveSnow() {
-		for (var i = height - 1; i >= 0; i--) {
-			if (Math.random() < fallingRate) {
-				snow[i][0] = '*';
-			}
-			if (Math.random() < fallingRate) {
-				snow[i][width-1] = '*';
-			}
+		// randomize initial snow
+		for (var i = 0; i <= 2*height; i++) {
+			moveSnow();
 		}
-		for (var j = width - 1; j >= 0; j--) {
-			if (Math.random() < fallingRate) {
-				snow[0][j] = '*';
+		
+		// start animation
+		function moveSnow() {
+			for (var i = height - 1; i >= 0; i--) {
+				if (Math.random() < fallingRate) {
+					snow[i][0] = '*';
+				}
+				if (Math.random() < fallingRate) {
+					snow[i][width-1] = '*';
+				}
 			}
-			if (Math.random() < meltingRate) {
-				snow[height-1][j] = ' ';
-			}
-		}
-		for (var i = height - 1; i >= 1; i--) {
 			for (var j = width - 1; j >= 0; j--) {
-				var rand = randomElement([-1, 0, 1]) + wind;
-				if (snow[i-1][j] == '*') {
-					snow[i][j+rand] = '*';
+				if (Math.random() < fallingRate) {
+					snow[0][j] = '*';
 				}
-				if (i <= height-snowhillHeight || snow[i][j-1]+snow[i][j]+snow[i][j+1] != '***') {
-					snow[i-1][j] = ' ';
+				if (Math.random() < meltingRate) {
+					snow[height-1][j] = ' ';
 				}
 			}
-		}
+			for (var i = height - 1; i >= 1; i--) {
+				for (var j = width - 1; j >= 0; j--) {
+					var rand = randomElement([-1, 0, 1]) + wind;
+					if (snow[i-1][j] == '*') {
+						snow[i][j+rand] = '*';
+					}
+					if (i <= height-snowhillHeight || snow[i][j-1]+snow[i][j]+snow[i][j+1] != '***') {
+						snow[i-1][j] = ' ';
+					}
+				}
+			}
 
-		snowBlock.innerText = snow.map(function(line){return line.slice(0, width).join('');}).join('\n');
+			snowBlock.innerText = snow.map(function(line){return line.slice(0, width).join('');}).join('\n');
+		}
+		clearInterval(moveSnowInterval);
+		moveSnowInterval = setInterval(moveSnow, 1000/speed);
+
 	}
-	clearInterval(moveSnowInterval);
-	moveSnowInterval = setInterval(moveSnow, 1000/speed);
+	window.onresize();
 
 }
